2ed7b3ce42b98723f26139a7e7cfbc6644a87810
VolodymyrLefthand <volodymyr.lefthand@gmail.com>
2025-05-27 00:19:44 +0200

    Task 3 zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .json

--- convertor/convertor.py
+++ convertor/convertor.py
@@ -31,6 +31,13 @@ def read_json(file_path):
         print(f"Error reading JSON:{e}")
         sys.exit(1)
 
+def write_json(data,file_path):
+    try:
+        with open(file_path,'w',encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    except Exception as e:
+        print(f"Error writing JSON {e}")
+        sys.exit(1)
 
 
 
